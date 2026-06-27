501aad3f9bf4f7350149f4bf7874e62cd4addc4a
KinhKhaVN <pebin0123@gmail.com>
2026-06-27 13:40:52 +0700

    Transposition characters

---
+++ Transposition/main.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <utility>
+
+
+void Transposition(char* text)
+{
+  int left = 0;
+  int right = strlen(text) - 1;
+
+  while (left < right)
+  {
+    std::swap(text[left], text[right]);
+    left++;
+    right--;
+  }
+}
+
+
+int main () {
+
+  char text[50] = "FLANK EAST ATTACK AT DAWN";
+
+  std::printf("Plaintext: %s\n", text);
+
+  Transposition(text);
+
+  std::printf("Ciphertext: %s\n", text);
+
+  return 0;
+}
